ff190911664e5f7daca6b5a61f99a45313666a1b
Diego Barcellos <diegocb81@gmail.com>
2020-11-22 00:00:10 -0300

    Atualizando o projeto

---
+++ projeto_contador_de_numeros_pares_e_impares/projeto_contador_de_numeros_pares_e_impares.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int limite, cont, num, qtdePar = 0, qtdeImpar = 0;
+    cout << "A sequência irá de 1 até quanto? ";
+    cin >> limite;
+    for (cont=1;cont<=limite;cont++)
+    {
+        cout << "Informe o " <<cont<< "o. número: ";
+        cin >> num;
+        if (num % 2 == 0)
+        {
+            qtdePar++;
+        }
+        else
+        {
+            qtdeImpar++;
+        }
+    }
+    cout << "Quantidade de números pares: " <<qtdePar<< "\n";
+    cout << "Quantidade de números ímpares: " <<qtdeImpar;
+}
